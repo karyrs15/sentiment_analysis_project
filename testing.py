a991e036720b2bcfa0836eaa1b86a805944acb47
Karina Reyes <karyrs1506@gmail.com>
2020-05-24 16:34:33 -0500

    Update testing file to work

--- testing.py
+++ testing.py
@@ -1,11 +1,30 @@
-import tensorflow as tf
 from keras.models import load_model
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+import nltk
+import re
+import pickle
+
+# download stopwords (we're gonna need it later)
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+# clean text to remove users, links and stopwords and then split it in tokens
+def clean_text(text):
+    text_cleaning_re = "@\S+|https?:\S+|http?:\S|[^A-Za-z0-9]+"
+    text = re.sub(text_cleaning_re, ' ', str(text).lower()).strip()
+    
+    tokens = text.split()
+    stop_words = set(stopwords.words('english'))
+    tokens = [w for w in tokens if w not in stop_words]
+    
+    return " ".join(tokens)
 
 # decode score prediction from the model, to be 0 or 1
 def decode_prediction(prediction):
     return 'Negative' if prediction < 0.5 else 'Positive'
+
+max_length = 50
 
 # load model
 model = load_model('model_final.h5')
@@ -14,10 +33,10 @@ with open('tokenizer.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
 # test model with a new query
-tweet = "im sad"
+tweet = "im happy!!!"
 
 # clean query text
-input_text = setup_file_sentiment_analysis.clean_text(tweet)
+input_text = clean_text(tweet)
 # tokenize and pad query test as in training
 input_text = pad_sequences(tokenizer.texts_to_sequences([input_text]),
                         maxlen = max_length)
@@ -27,5 +46,5 @@ prediction = model.predict([input_text])[0]
 # get decode prediction
 label = decode_prediction(prediction)
 
-print("Tweet: \n\n{}\n".format(query_text))
+print("Tweet: \n\n{}\n".format(tweet))
 print("Score: {} Label: {}".format(prediction, label))
